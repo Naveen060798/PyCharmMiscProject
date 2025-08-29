33872f6bef29748c32544e8b01bfac38888f1ab2
Naveen060798 <naveenkumar671998@gmail.com>
2025-08-28 18:29:34 -0700

    changes

--- Longest list.py
+++ Longest list.py
@@ -1,2 +1,14 @@
-l=[[1,2,3],[4,5,6,7],[8,9,10,11,12,13]]
-print(f"Maximum list: {max(l, key=sum)}")
+# Sample nested list
+nested_list = [[1, 2, 3], [4, 5], [6, 7, 8, 9], [10]]
+
+# Assume first sublist is largest initially
+largest_list = nested_list[0]
+
+# Loop through each sublist
+for sublist in nested_list:
+    if len(sublist) > len(largest_list):
+        largest_list = sublist
+
+# Print result
+print("The largest list is:", largest_list)
+print("Length of largest list:", len(largest_list))
